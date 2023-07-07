8480b7aec2153d8dcfda0100ffde177c4741197f
ToHoanghuy <127731812+ToHoanghuy@users.noreply.github.com>
2023-07-07 17:40:46 +0700

    as

--- LuyenCode_DK09/LuyenCode_DK09.py
+++ LuyenCode_DK09/LuyenCode_DK09.py
@@ -1,7 +1,7 @@
 
 year = int(input())
 
-if (year>0 or year<=100000):
+if (year>0 and year<=100000):
     if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
         print("YES")
     else:
--- LuyenCode_DK10/LuyenCode_DK10.py
+++ LuyenCode_DK10/LuyenCode_DK10.py
@@ -1,1 +1,16 @@
 
+month, year = [int(x) for x in input().split()]
+
+if(month>=1 and month<=12 and year>0 and year<=100000):
+    if (month==1 or month==3 or month ==5 or month==7 or month==8 or month==10 or month==12):
+        print(31)
+    elif (month==4 or month==6 or month== 9 or month==11):
+        print(30)
+    elif(month==2):
+        if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
+            print(29)
+        else:
+            print(28)
+else: 
+    print("INVALID")
+
